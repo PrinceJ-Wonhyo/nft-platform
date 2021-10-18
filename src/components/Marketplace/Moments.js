d2d95fdf34e421bf50ecb9cba7444ea4b3a535e5
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-10-18 23:52:16 +0900

    Add cards for featured NFTs

---
+++ src/components/Marketplace/Moments.js
@@ -0,0 +1,201 @@
+import React, { Component, useState } from "react";
+
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+
+export default class Moments extends Component {
+    render() {
+      const item = [
+        {
+          id: 1,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 2,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 3,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 4,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 5,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 6,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 7,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 8,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 9,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+        {
+          id: 10,
+          itemImg: "./assets/NFTKOREA.png",
+          hot: "./assets/hot.png",
+          name: "Card Name",
+          desc: "Dunk - Base Set",
+          series: "(Series 2)",
+          rarity: "Common #/4000",
+          badge: "./assets/logo.png",
+          badge2: "./assets/logo-1.png",
+          lowPrice: 8.0,
+          avgSale: 6.79,
+        },
+      ];
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      };
+      return (
+        <div>
+          <header>
+            <h2> What's Hot </h2>
+            <Link>Explore Marketplace</Link>
+          </header>
+          <Slider {...settings}>
+            {item.map(
+              ({
+                itemImg,
+                hot,
+                name,
+                desc,
+                series,
+                rarity,
+                badge,
+                badge2,
+                lowPrice,
+                avgSale,
+                id,
+              }) => (
+                <div className="card" key={id}>
+                  <div className="hot-item">
+                    <img src={itemImg} width="106" height="106" alt="" />
+                    <div className="item-copy">
+                      <img src={hot} alt="" width="50" height="19" />
+                      <p>{name}</p>
+                      <p>{desc}</p>
+                      <p>{series}</p>
+                      <p>
+                        {rarity}
+                        <span>LE</span>
+                      </p>
+                      <img src={badge} alt="" width="23" height="23" />
+                      <img src={badge2} alt="" width="23" height="23" />
+                    </div>
+                  </div>
+                  <footer>
+                    <div>
+                      <p>Lowest Ask</p>
+                      <p>USD ${lowPrice}</p>
+                    </div>
+                    <div>
+                      <p>Avg Sale in last 14 Days</p>
+                      <p>${avgSale}</p>
+                    </div>
+                  </footer>
+                </div>
+              )
+            )}
+          </Slider>
+        </div>
+      );
+    }
+  }
